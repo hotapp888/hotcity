77b133ebb715f27d1c8a328b0c9545ae6ecb08d0
spacexu <spacexu@2bai.com.cn>
2016-12-01 18:33:33 +0800

    接入hotapp小程序统计

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ HotCity城市选择器由HotApp小程序统计团队开发并开源：https://gi
 小程序交流学习QQ群173063969
 */
 //app.js
+var hotapp = require('util/hotapp.js');
 App({
   onLaunch: function () {
-    
-  },
-  globalData:{
-    areaInfo:''
+    //一行代码接入HotApp小程序统
+    //统计接入成功后 会自动统计 今日启动数 昨日启动数 今日新增用户 昨日新增用户  今日活跃 昨日活跃数 今日累计用户
+    hotapp.init("hotapp22259962");
   }
 })
